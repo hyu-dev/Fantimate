303540e5ed2d5192ba074c8710f33fec4fd4600e
hyu-dev <hyu630115@gmail.com>
2021-04-07 11:08:48 +0900

    마스터 버전 맞추기 위해 업로드

--- src/main/java/com/kh/fantimate/store/controller/StoreController.java
+++ src/main/java/com/kh/fantimate/store/controller/StoreController.java
@@ -1,6 +1,10 @@
 package com.kh.fantimate.store.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,15 +16,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kh.fantimate.store.model.service.StoreService;
+import com.kh.fantimate.store.model.vo.Store;
 import com.kh.fantimate.store.model.vo.StoreCategory;
 import com.kh.fantimate.store.model.vo.StoreCollection;
+import com.kh.fantimate.store.model.vo.StoreInfo;
 
 @Controller
 @RestController
@@ -191,6 +198,46 @@ public class StoreController {
 		if(list == null || list.size() < 1) list = new ArrayList<>();
 			
 		return list;   // path 경로로 가는데 앞에  /sp 는 삭제
+	}
+	
+	@PostMapping("/insert")
+	public ModelAndView insertStore(ModelAndView mv,
+									Store store,
+									StoreCategory storeCate,
+									StoreInfo storeInfo,
+									@RequestParam(value="attClName") MultipartFile file,
+									HttpServletRequest request) {
+		
+		System.out.println(store);
+		System.out.println(storeCate);
+		System.out.println(storeInfo);
+		System.out.println(file.getName());
+		System.out.println(file.getOriginalFilename());
+		return mv;
+	}
+	
+	// 파일 리네임 후 저장
+	public String saveFile(MultipartFile file, HttpServletRequest request) {
+		String root = request.getSession().getServletContext().getRealPath("resources");
+		String savePath = root + "\\uploadFiles";
+		File folder = new File(savePath);
+		if(!folder.exists()) folder.mkdirs();	// -> 해당 경로가 존재하지 않는다면 디렉토리 생성
+		
+		// 파일명 리네임 규칙 "년월일시분초_랜덤값.확장자"
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+		String originalFileName = file.getOriginalFilename();
+		String renameFileName = sdf.format(new Date()) + "_" 
+							+ (int)(Math.random() * 100000)
+							+ originalFileName.substring(originalFileName.lastIndexOf("."));
+		String renamePath = folder + "\\" + renameFileName; 	// 저장하고자 하는 경로 + 파일명
+		
+		try {
+			file.transferTo(new File(renamePath));
+			// => 업로드 된 파일(MultipartFile)이 rename명으로 서버에 저장
+		} catch (IllegalStateException | IOException e) {
+			System.out.println("파일 업로드 에러 : " + e.getMessage());
+		}	
+		return renameFileName;
 	}
 	
 		
