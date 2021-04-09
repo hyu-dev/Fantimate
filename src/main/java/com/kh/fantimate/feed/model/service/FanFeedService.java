1a198e7f808b9426041fb76c1eef3955ea297445
고성현 <shkow9568@gmail.com>
2021-04-09 10:19:02 +0900

    게시글 작성 기능 + mybatis-config 커밋

--- src/main/java/com/kh/fantimate/feed/model/service/FanFeedService.java
+++ src/main/java/com/kh/fantimate/feed/model/service/FanFeedService.java
@@ -1,5 +1,12 @@
 package com.kh.fantimate.feed.model.service;
 
+import java.util.List;
+
+import com.kh.fantimate.common.model.vo.Attachment;
+import com.kh.fantimate.feed.model.vo.Feed;
+
 public interface FanFeedService {
 
+	// 게시글 작성
+	public int insertFeed(Feed f, List<Attachment> attList);
 }
