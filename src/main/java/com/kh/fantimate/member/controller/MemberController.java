bfa56099d66c2d442f89974719be72bb9f7b2817
seungahh <poochii96@naver.com>
2021-05-03 13:29:42 +0900

    [메인페이지] - 수정

--- src/main/java/com/kh/fantimate/member/controller/MemberController.java
+++ src/main/java/com/kh/fantimate/member/controller/MemberController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.support.SessionStatus;
 import com.google.gson.Gson;
 import com.kh.fantimate.common.model.vo.Attachment;
 import com.kh.fantimate.main.model.vo.FavoriteArtist;
+import com.kh.fantimate.main.model.vo.QuitSubscribeCollection;
 import com.kh.fantimate.main.model.vo.SubscribeArtist;
 import com.kh.fantimate.member.model.service.MemberService;
 import com.kh.fantimate.member.model.vo.Admin;
@@ -180,7 +181,6 @@ public class MemberController {
 			if(loginUser.getClassifyMem() == 1 && loginUser.getStatus().equals("Y")) {
 				
 				List<UserCollection> user = (ArrayList<UserCollection>)mService.loginUser(loginUser.getId());
-				// 회원의 구독 정보 (세션에 담기) --> 널 && 유저 1일 경우 슬라이드 안 보이게 
 				List<SubscribeArtist> subArtist = (ArrayList<SubscribeArtist>)mService.selectSubA(loginUser.getId());
 				System.out.println("회원 구독 정보오오:" + subArtist);
 				session.setAttribute("subArtist", subArtist);
@@ -221,6 +221,24 @@ public class MemberController {
 
 	}
 	
+	// 회원의 구독 아티스트 세션에 담기
+	@GetMapping("/sub")
+	public String sub(HttpSession session, SubscribeArtist sb) {
+		
+		SubscribeArtist s = new SubscribeArtist();
+		Member loginUser = (Member)session.getAttribute("loginUser");
+		String user = loginUser.getId();
+		
+		// 회원의 구독 정보 (세션에 담기) --> 널 && 유저 1일 경우 슬라이드 안 보이게 
+		List<SubscribeArtist> subArtist = (ArrayList<SubscribeArtist>)mService.selectSubA(loginUser.getId());
+		System.out.println("회원 구독 정보오오:" + subArtist);
+		session.setAttribute("subArtist", subArtist);
+		
+		return "common/main"; 
+		
+	}
+	
+
 	@GetMapping("/logout")
 	public String logout(SessionStatus status) {
 		
